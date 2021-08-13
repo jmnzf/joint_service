cabead8cb25e7faa5471e3973a8d1a6fb2bdf42c
Fredy Jimenez <fjimenez@webtic.co>
2021-08-13 10:56:51 -0500

    Subiendo cambios en servicio error id 2

--- application/controllers/ItemsGroup.php
+++ application/controllers/ItemsGroup.php
@@ -149,8 +149,8 @@ class ItemsGroup extends REST_Controller {
                                     mga_acctin = :mga_acctin,
                                     mga_acct_out = :mga_acct_out,
                                     mga_acct_inv = :mga_acct_inv,
-                                    mga_acct_stockp = :mga_acct_stockp,
                                     mga_acct_stockn = :mga_acct_stockn,
+                                    mga_acct_stockp = :mga_acct_stockp,
                                     mga_acct_redu = :mga_acct_redu,
                                     mga_acct_amp = :mga_acct_amp,
                                     mga_acct_cost = :mga_acct_cost,
@@ -172,6 +172,7 @@ class ItemsGroup extends REST_Controller {
         ':mga_acct_amp'  => $Data['mga_acct_amp'],
         ':mga_acct_cost'  => $Data['mga_acct_cost'],
         ':mga_enabled'  => $Data['mga_enabled'],
+        ':mga_id'  => $Data['mga_id'],
       ));
 
       
