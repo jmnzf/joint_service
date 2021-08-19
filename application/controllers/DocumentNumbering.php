b1e2d9c1ed10bd6a94bcde410956dcf46c6f66f1
Fredy Jimenez <fjimenez@webtic.co>
2021-08-19 11:52:13 -0500

    Actualizando ultimo numero

--- application/controllers/DocumentNumbering.php
+++ application/controllers/DocumentNumbering.php
@@ -221,7 +221,9 @@ class DocumentNumbering extends REST_Controller {
   // Obtener numeracion de documento
   public function getDocumentNumbering_get(){
 
-        $sqlSelect = " SELECT * FROM pgdn";
+        // $sqlSelect = " SELECT * FROM pgdn";
+        $sqlSelect = "SELECT pgs_id, pgs_id_doc_type, pgs_num_name, pgs_first_num, pgs_last_num, pgs_pref_num, pgs_cancel, 
+        pgs_is_due, pgs_doc_date, pgs_doc_due_date, pgs_enabled, (select max(dvc_docnum) ultimo_numero from dvct t0 where t0.dvc_series = pgs_id)  as ultimo_numero FROM pgdn";
 
         $resSelect = $this->pedeo->queryTable($sqlSelect, array());
 
