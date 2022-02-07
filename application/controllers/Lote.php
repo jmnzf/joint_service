b594589f3a8601f1e735fc086c93e5e02fd38eec
Alberto Imitola <aimitola@webtic.co>
2022-02-07 17:08:38 -0500

    se agrego la funcion de ver los lotes agregados

--- application/controllers/Lote.php
+++ application/controllers/Lote.php
@@ -22,32 +22,72 @@ class Lote extends REST_Controller {
 
 	}
 
+	public function getLotes_get(){
+	
+		$result = $this->pedeo->queryTable("SELECT * FROM lote", array());
+		// VALIDAR RETORNO DE DATOS DE LA CONSULTA.
+		if(isset($result[0])){
+			//
+			  $response = array(
+				'error'  => false,
+				'data'   => $result,
+				'mensaje'=> ''
+			);
+		}
+		//
+		 $this->response($response);
+	}
+
 	public function createLote_post(){
 
-      $Data = $this->post();
+     	 $Data = $this->post();
+		  if( (!isset($Data['ote_code']) AND empty($Data['ote_code'])) OR
+				!isset($Data['ote_createdate']) OR
+				!isset($Data['ote_createdate'])
+			){
+			$this->response(array(
+				'error'  => true,
+				'data'   => [],
+				'mensaje'=>'La informacion enviada no es valida'
+			), REST_Controller::HTTP_BAD_REQUEST);
 
-			$sqlInsert = "INSERT INTO lote(ote_code, ote_createdate, ote_duedate)
-                  	VALUES(:ote_code, :ote_createdate, :ote_duedate)";
-
-			$resInsert = $this->pedeo->insertRow($sqlInsert, array(
-        ':ote_code' => isset($value['ote_code'])?$value['ote_code']:NULL,
-        ':ote_createdate' => $this->validateDate($value['ote_createdate']),
-        ':ote_duedate' => $this->validateDate($value['ote_duedate'])
-			));
-
-			if(is_numeric($resInsert) && $resInsert > 0 ){
-			}else{
-				$respuesta = array(
-					'error'   => true,
-					'data' => array(),
-					'mensaje'	=> 'No se pudo registrar la tasa'
-				);
-
-				$this->response($respuesta);
-
-				return;
-
+			return ;
 			}
+
+			if($Data['ote_createdate']>$Data['ote_duedate']){
+
+				$this->response(array(
+					'error'  => true,
+					'data'   => [],
+					'mensaje'=>'Las fechas no son validas'
+				), REST_Controller::HTTP_BAD_REQUEST);
+	
+				return ;
+			}
+
+		$sqlInsert = "INSERT INTO lote(ote_code, ote_createdate, ote_duedate)
+				VALUES(:ote_code, :ote_createdate, :ote_duedate)";
+
+		$resInsert = $this->pedeo->insertRow($sqlInsert, array(
+        ':ote_code' => !isset($value['ote_code'])?$Data['ote_code']:NULL,
+        ':ote_createdate' => ($this->validateDate($Data['ote_createdate']))?$Data['ote_createdate']:NULL,
+        ':ote_duedate' => ($this->validateDate($Data['ote_duedate']))? $Data['ote_duedate']:NULL
+		));
+
+		if(is_numeric($resInsert) && $resInsert > 0 ){
+			
+		}else{
+			$respuesta = array(
+				'error'   => true,
+				'data' => $resInsert,
+				'mensaje'	=> 'No se pudo registrar la tasa'
+			);
+
+			$this->response($respuesta);
+
+			return;
+
+		}
 
       $respuesta = array(
       'error' => false,
