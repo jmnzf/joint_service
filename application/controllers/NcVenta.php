41f420eee65a033b859cec2c172ddf9fc5a964d0
Alberto Imitola <aimitola@webtic.co>
2022-03-11 10:17:49 -0500

    se quito el sombreado en cada documento

--- application/controllers/NcVenta.php
+++ application/controllers/NcVenta.php
@@ -171,7 +171,7 @@ class NcVenta extends REST_Controller {
             </th>
             <th>
                 <p>NOTA CREDITO DE VENTA</p>
-                <p class="fondo">'.$consecutivo.'</p>
+                <p class="">'.$consecutivo.'</p>
 
             </th>
         </tr>
@@ -183,7 +183,7 @@ class NcVenta extends REST_Controller {
         <table width="100%" style="vertical-align: bottom; font-family: serif;
             font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
             <tr>
-                <th class="fondo" width="33%">Pagina: {PAGENO}/{nbpg}  Fecha: {DATE j-m-Y}  </th>
+                <th class="" width="33%">Pagina: {PAGENO}/{nbpg}  Fecha: {DATE j-m-Y}  </th>
             </tr>
         </table>';
 
@@ -272,13 +272,13 @@ class NcVenta extends REST_Controller {
 
         <table class="borde" style="width:100%">
         <tr>
-          <th class="fondo">ITEM</th>
-          <th class="fondo">REFERENCIA</th>
-          <th class="fondo">UNIDAD</th>
-          <th class="fondo">PRECIO</th>
-          <th class="fondo">CANTIDAD</th>
-          <th class="fondo">IVA</th>
-          <th class="fondo">TOTAL</th>
+          <th class="">ITEM</th>
+          <th class="">REFERENCIA</th>
+          <th class="">UNIDAD</th>
+          <th class="">PRECIO</th>
+          <th class="">CANTIDAD</th>
+          <th class="">IVA</th>
+          <th class="">TOTAL</th>
         </tr>
       	'.$totaldetalle.'
         </table>
@@ -286,7 +286,7 @@ class NcVenta extends REST_Controller {
         <table width="100%" style="vertical-align: bottom; font-family: serif;
             font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
             <tr>
-                <th class="fondo">
+                <th class="">
                     <p></p>
                 </th>
             </tr>
@@ -318,7 +318,7 @@ class NcVenta extends REST_Controller {
         <table width="100%" style="vertical-align: bottom; font-family: serif;
             font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
             <tr>
-                <th style="text-align: left;" class="fondo">
+                <th style="text-align: left;" class="">
                     <p>'.$formatter->toWords($contenidoNcV[0]['totaldoc'],2)." ".$contenidoNcV[0]['nombremoneda'].'</p>
                 </th>
             </tr>
@@ -336,7 +336,7 @@ class NcVenta extends REST_Controller {
         $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
         $mpdf->WriteHTML($html,\Mpdf\HTMLParserMode::HTML_BODY);
 
-
+		$filename = 'Doc.pdf';
         $mpdf->Output('Doc.pdf', 'D');
 
 				header('Content-type: application/force-download');
