f1ede3e3e9ca148053f8713f99fac5634b2f6428
Administrador <chernandez@webtic.co>
2022-05-02 15:50:55 -0500

    actualizando local

--- application/controllers/MaterialList.php
+++ application/controllers/MaterialList.php
@@ -18,14 +18,16 @@ class MaterialList extends REST_Controller {
 		$this->load->database();
 		$this->pdo = $this->load->database('pdo', true)->conn_id;
     $this->load->library('pedeo', [$this->pdo]);
+		$this->load->library('generic');
 
 	}
 
-	public function createMaterialList_get(){
+	public function createMaterialList_post(){
 
-		$request = $this->get();
+		$request = $this->post();
 
-		if( !isset($request['Pgu_Role']) ){
+
+		if(!isset($Data['detail'])){
 
 			$respuesta = array(
 				'error' => true,
@@ -38,8 +40,257 @@ class MaterialList extends REST_Controller {
 			return;
 		}
 
+		$ContenidoDetalle = json_decode($Data['detail'], true);
 
 
+		if(!is_array($ContenidoDetalle)){
+				$respuesta = array(
+					'error' => true,
+					'data'  => array(),
+					'mensaje' =>'No se encontro el detalle de la lista'
+				);
+
+				$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+				return;
+		}
+
+		// SE VALIDA QUE EL DOCUMENTO TENGA CONTENIDO
+		if(!intval(count($ContenidoDetalle)) > 0 ){
+				$respuesta = array(
+					'error' => true,
+					'data'  => array(),
+					'mensaje' =>'Documento sin detalle'
+				);
+
+				$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+				return;
+		}
+		//
+
+		//Obtener Carpeta Principal del Proyecto
+		$sqlMainFolder = " SELECT * FROM params";
+		$resMainFolder = $this->pedeo->queryTable($sqlMainFolder, array());
+
+		if(!isset($resMainFolder[0])){
+				$respuesta = array(
+				'error' => true,
+				'data'  => array(),
+				'mensaje' =>'No se encontro la caperta principal del proyecto'
+				);
+
+				$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+				return;
+		}
+		//
+
+		$sqlInsert = "INSERT INTO prlm(rlm_item_code, rlm_quantity, rlm_bom_type, rlm_whscode, rlm_pricelist, rlm_nom_dist,
+			 												rlm_project, rlm_comment, rlm_costoprod, rlm_total, rlm_createat, rlm_createby)
+															VALUES(:rlm_item_code, :rlm_quantity, :rlm_bom_type, :rlm_whscode, :rlm_pricelist, :rlm_nom_dist,
+															:rlm_project, :rlm_comment, :rlm_costoprod, :rlm_total, :rlm_createat, :rlm_createby)";
+
+		$resInsert = $this->pedeo->insertRow(	$sqlInsert, array(
+			':rlm_item_code' => isset($Data['rlm_item_code'])?$Data['rlm_item_code']:NULL,
+			':rlm_quantity' => is_numeric($Data['rlm_quantity'])?$Data['rlm_quantity']:0,
+			':rlm_bom_type' => isset($Data['rlm_bom_type'])?$Data['rlm_bom_type']:NULL,
+			':rlm_whscode' => isset($Data['rlm_whscode'])?$Data['rlm_whscode']:NULL,
+			':rlm_pricelist' => is_numeric($Data['rlm_pricelist'])?$Data['rlm_pricelist']:0,
+			':rlm_nom_dist' => isset($Data['rlm_nom_dist'])?$Data['rlm_nom_dist']:NULL,
+			':rlm_project' => isset($Data['rlm_project'])?$Data['rlm_project']:NULL,
+			':rlm_comment' => isset($Data['rlm_comment'])?$Data['rlm_comment']:NULL,
+			':rlm_costoprod' => is_numeric($Data['rlm_costoprod'])?$Data['rlm_costoprod']:0,
+			':rlm_total' => is_numeric($Data['rlm_total'])?$Data['rlm_total']:0,
+			':rlm_createat' => $this->generic->validateDate($Data['rlm_createat'])?$Data['rlm_createat']:NULL,
+			':rlm_createby' => isset($Data['rlm_createby'])?$Data['rlm_createby']:NULL
+		));
+
+		if( is_numeric($resInsert) && $resInsert > 0 ){
+
+			 $this->pedeo->trans_begin();
+
+			foreach ($ContenidoDetalle as $key => $detail) {
+
+				$sqlInsertDetail = "INSERT INTO public.rlm1(lm1_iddoc, lm1_linenum, lm1_type, lm1_itemcode, lm1_itemname, lm1_quantity,
+					 									lm1_uom, lm1_whscode, lm1_emission_method, lm1_standard_cost, lm1_standard_costt, lm1_pricelist,
+														lm1_price, lm1_total, lm1_comment)VALUES(:lm1_iddoc, :lm1_linenum, :lm1_type, :lm1_itemcode, :lm1_itemname, :lm1_quantity,
+														:lm1_uom, :lm1_whscode, :lm1_emission_method, :lm1_standard_cost, :lm1_standard_costt, :lm1_pricelist,
+														:lm1_price, :lm1_total, :lm1_comment)";
+
+				$resInsertDetail = $this->pedeo->insertRow($sqlInsertDetail, array(
+
+					':lm1_iddoc' => $resInsert,
+					':lm1_linenum' => is_numeric($Data['lm1_linenum'])?$Data['lm1_linenum']:0,
+					':lm1_type' => is_numeric($Data['lm1_type'])?$Data['lm1_type']:0,
+					':lm1_itemcode' => isset($Data['lm1_itemcode'])?$Data['lm1_itemcode']:NULL,
+					':lm1_itemname' => isset($Data['lm1_itemname'])?$Data['lm1_itemname']:NULL,
+					':lm1_quantity' => is_numeric($Data['lm1_quantity'])?$Data['lm1_quantity']:NULL,
+					':lm1_uom' => isset($Data['lm1_uom'])?$Data['lm1_uom']:NULL,
+					':lm1_whscode' =>  isset($Data['lm1_whscode'])?$Data['lm1_whscode']:NULL,
+					':lm1_emission_method' => is_numeric($Data['lm1_emission_method'])?$Data['lm1_emission_method']:NULL,
+					':lm1_standard_cost' => is_numeric($Data['lm1_standard_cost'])?$Data['lm1_standard_cost']:NULL,
+					':lm1_standard_costt' => is_numeric($Data['lm1_standard_costt'])?$Data['lm1_standard_costt']:NULL,
+					':lm1_pricelist' =>is_numeric($Data['lm1_pricelist'])?$Data['lm1_pricelist']:NULL,
+					':lm1_price' => is_numeric($Data['lm1_price'])?$Data['lm1_price']:NULL,
+					':lm1_total' => is_numeric($Data['lm1_total'])?$Data['lm1_total']:NULL,
+					':lm1_comment' =>isset($Data['lm1_comment'])?$Data['lm1_comment']:NULL
+				));
+
+
+				if(is_numeric($resInsertDetail) && $resInsertDetail > 0){
+
+				}else{
+					$this->pedeo->trans_rollback();
+
+					$respuesta = array(
+						'error'   => true,
+						'data' => $resInsertDetail,
+						'mensaje'	=> 'No se pudo registrar la lista'
+					);
+
+					 $this->response($respuesta);
+
+					 return;
+				}
+			}
+
+			$this->pedeo->trans_commit();
+
+			$respuesta = array(
+				'error' => false,
+				'data' => $resInsert,
+				'mensaje' =>'Lista registrada con exito'
+			);
+
+		}else{
+
+			$respuesta = array(
+				'error'   => true,
+				'data'    => $resInsert,
+				'mensaje'	=> 'No se pudo crear la cotización'
+			);
+		}
 		$this->response($respuesta);
 	}
+
+
+
+	//OBTENER LISTA DE MATERIALES POR ID
+	public function getMaterialListById_get(){
+
+				$Data = $this->get();
+
+				if(!isset($Data['rlm_id'])){
+
+					$respuesta = array(
+						'error' => true,
+						'data'  => array(),
+						'mensaje' =>'La informacion enviada no es valida'
+					);
+
+					$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+					return;
+				}
+
+				$sqlSelect = " SELECT * FROM prlm WHERE rlm_id =:rlm_id";
+
+				$resSelect = $this->pedeo->queryTable($sqlSelect, array(":rlm_id" => $Data['rlm_id']));
+
+				if(isset($resSelect[0])){
+
+					$respuesta = array(
+						'error' => false,
+						'data'  => $resSelect,
+						'mensaje' => '');
+
+				}else{
+
+						$respuesta = array(
+							'error'   => true,
+							'data' => array(),
+							'mensaje'	=> 'busqueda sin resultados'
+						);
+
+				}
+
+				 $this->response($respuesta);
+	}
+
+	//OBTENER LISTA DE MATERIALES DETALLE POR ID
+	public function getMaterialListDetail_get(){
+
+				$Data = $this->get();
+
+				if(!isset($Data['lm1_iddoc'])){
+
+					$respuesta = array(
+						'error' => true,
+						'data'  => array(),
+						'mensaje' =>'La informacion enviada no es valida'
+					);
+
+					$this->response($respuesta, REST_Controller::HTTP_BAD_REQUEST);
+
+					return;
+				}
+
+				$sqlSelect = "SELECT * FROM rlm1 WHERE lm1_iddoc =:lm1_iddoc";
+
+				$resSelect = $this->pedeo->queryTable($sqlSelect, array(":lm1_iddoc" => $Data['lm1_iddoc']));
+
+				if(isset($resSelect[0])){
+
+					$respuesta = array(
+						'error' => false,
+						'data'  => $resSelect,
+						'mensaje' => '');
+
+				}else{
+
+						$respuesta = array(
+							'error'   => true,
+							'data' => array(),
+							'mensaje'	=> 'busqueda sin resultados'
+						);
+
+				}
+
+				 $this->response($respuesta);
+	}
+
+	//OBTENER LISTA DE MATERIALES
+	public function getMaterialList_get(){
+
+				$Data = $this->get();
+
+
+
+				$sqlSelect = " SELECT * FROM prlm";
+
+				$resSelect = $this->pedeo->queryTable($sqlSelect, array());
+
+				if(isset($resSelect[0])){
+
+					$respuesta = array(
+						'error' => false,
+						'data'  => $resSelect,
+						'mensaje' => '');
+
+				}else{
+
+						$respuesta = array(
+							'error'   => true,
+							'data' => array(),
+							'mensaje'	=> 'busqueda sin resultados'
+						);
+
+				}
+
+				 $this->response($respuesta);
+	}
+
+
+
 }
