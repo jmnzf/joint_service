5b8dc96995651544d0a5c9f614c63775c1073675
Alberto Imitola <aimitola@webtic.co>
2022-12-27 10:17:48 -0500

    ajuste en servicios de revalorizacion

--- application/controllers/InventoryRevaluation.php
+++ application/controllers/InventoryRevaluation.php
@@ -1,16 +1,18 @@
 <?php
 // REVALORIZACION DE INVENTARIO
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require_once(APPPATH.'/libraries/REST_Controller.php');
+require_once(APPPATH . '/libraries/REST_Controller.php');
 use Restserver\libraries\REST_Controller;
 
-class InventoryRevaluation extends REST_Controller {
+class InventoryRevaluation extends REST_Controller
+{
 
 	private $pdo;
 
-	public function __construct(){
+	public function __construct()
+	{
 
 		header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
 		header("Access-Control-Allow-Headers: Content-Type, Content-Length, Accept-Encoding");
@@ -19,62 +21,70 @@ class InventoryRevaluation extends REST_Controller {
 		parent::__construct();
 		$this->load->database();
 		$this->pdo = $this->load->database('pdo', true)->conn_id;
-    $this->load->library('pedeo', [$this->pdo]);
+		$this->load->library('pedeo', [$this->pdo]);
 
 	}
 
-  //CREAR NUEVA REVALORIZACION
-  public function getinventoryRevaluation_get()
-  {
-	$sqlSelect = " SELECT * FROM diri";
+	//CREAR NUEVA REVALORIZACION
+	public function getinventoryRevaluation_get()
+	{
+		$sqlSelect = " SELECT * FROM diri";
 
-	$resSelect = $this->pedeo->queryTable($sqlSelect, array());
+		$resSelect = $this->pedeo->queryTable($sqlSelect, array());
 
-	if(isset($resSelect[0])){
+		if (isset($resSelect[0])) {
 
-	  $respuesta = array(
-		'error' => false,
-		'data'  => $resSelect,
-		'mensaje' => '');
+			$respuesta = array(
+				'error' => false,
+				'data' => $resSelect,
+				'mensaje' => ''
+			);
 
-	}else{
+		} else {
 
-		$respuesta = array(
-		  'error'   => true,
-		  'data' => array(),
-		  'mensaje'	=> 'busqueda sin resultados'
-		);
+			$respuesta = array(
+				'error' => true,
+				'data' => array(),
+				'mensaje' => 'busqueda sin resultados'
+			);
 
+		}
+
+		$this->response($respuesta);
 	}
 
-	 $this->response($respuesta);
-  }
-
-  public function createRevaluation_post(){
-
+	public function createRevaluation_post()
+	{
 		$Data = $this->post();
-
-		$sqlInset = "INSERT INTO diri (iri_card_code, iri_card_name, iri_docdate, iri_duedate, iri_duedev) VALUES(:iri_card_code, :iri_card_name, :iri_docdate, :iri_duedate, :iri_duedev)";
+		$sqlInset = "INSERT INTO diri (iri_docnum,iri_doctype, iri_series, iri_cardcode, iri_cardname, iri_docdate, iri_duedate, iri_duedev, iri_comment, iri_currency, iri_slpcode, iri_empid) VALUES(:iri_docnum, :iri_doctype, :iri_series, :iri_cardcode, :iri_cardname, :iri_docdate, :iri_duedate, :iri_duedev, :iri_comment, :iri_currency, :iri_slpcode, :iri_empid)";
 		$this->pedeo->trans_begin();
 		$resInsert = $this->pedeo->insertRow($sqlInset, array(
-			":iri_card_code" => $Data['iri_card_code'], 
-			":iri_card_name" => $Data['iri_card_name'], 
-			":iri_docdate" => $Data['iri_docdate'], 
-			":iri_duedate" => $Data['iri_duedate'], 
-			":iri_duedev" => $Data['iri_duedev']
-			));
+			":iri_docnum" => $Data['iri_docnum'],
+			":iri_doctype" => $Data['iri_doctype'],
+			":iri_series" => $Data['iri_series'],
+			":iri_cardcode" => $Data['iri_cardcode'],
+			":iri_cardname" => $Data['iri_cardname'],
+			":iri_docdate" => $Data['iri_docdate'],
+			":iri_duedate" => $Data['iri_duedate'],
+			":iri_duedev" => $Data['iri_duedev'],
+			":iri_comment" => $Data['iri_comment'],
+			":iri_currency" => $Data['iri_currency'],
+			":iri_slpcode" => $Data['iri_slpcode'],
+			":iri_empid" => $Data['iri_empid'],
+		)
+		);
 
-		 if(is_numeric($resInsert)  && $resInsert > 0){
+		if (is_numeric($resInsert) && $resInsert > 0) {
 
-			$sqlDetail = "INSERT INTO iri1 (ri1_item_code,ri1_item_name,ri1_wsh_code,ri1_quantity,ri1_actual_cost,ri1_new_cost,ri1_increase_account,ri1_declining_account,ri1_ccost,ri1_ubussines,ri1_project, ri1_docentry, ri1_ubication, ri1_total) VALUES
-						(:ri1_item_code,:ri1_item_name,:ri1_wsh_code,:ri1_quantity,:ri1_actual_cost,:ri1_new_cost,:ri1_increase_account,:ri1_declining_account,:ri1_ccost,:ri1_ubussines,:ri1_project, :ri1_docentry , :ri1_ubication, :ri1_total)";
+			$sqlDetail = "INSERT INTO iri1 (ri1_itemcode,ri1_itemname,ri1_whscode,ri1_quantity,ri1_actualcost,ri1_newcost,ri1_increase_account,ri1_declining_account,ri1_costcode,ri1_ubusiness,ri1_project, ri1_docentry, ri1_ubication, ri1_linetotal) VALUES
+						(:ri1_itemcode,:ri1_itemname,:ri1_whscode,:ri1_quantity,:ri1_actualcost,:ri1_newcost,:ri1_increase_account,:ri1_declining_account,:ri1_costcode,:ri1_ubusiness,:ri1_project, :ri1_docentry , :ri1_ubication, :ri1_linetotal)";
 
 			$ContenidoDetalle = json_decode($Data['detail'], true);
 
 			if (!is_array($ContenidoDetalle)) {
 				$respuesta = array(
 					'error' => true,
-					'data'  => array(),
+					'data' => array(),
 					'mensaje' => 'No se encontro el detalle'
 				);
 
@@ -86,7 +96,7 @@ class InventoryRevaluation extends REST_Controller {
 			if (!intval(count($ContenidoDetalle)) > 0) {
 				$respuesta = array(
 					'error' => true,
-					'data'  => array(),
+					'data' => array(),
 					'mensaje' => 'Documento sin detalle'
 				);
 
@@ -95,22 +105,23 @@ class InventoryRevaluation extends REST_Controller {
 				return;
 			}
 			foreach ($ContenidoDetalle as $key => $detail) {
-				$resInsertDetail = $this->pedeo->insertRow($sqlDetail,array(
-					":ri1_item_code" => $detail['ri1_item_code'],
-					":ri1_item_name" => $detail['ri1_item_name'],
-					":ri1_wsh_code" => $detail['ri1_wsh_code'],
+				$resInsertDetail = $this->pedeo->insertRow($sqlDetail, array(
+					":ri1_itemcode" => $detail['ri1_itemcode'],
+					":ri1_itemname" => $detail['ri1_itemname'],
+					":ri1_whscode" => $detail['ri1_whscode'],
 					":ri1_quantity" => $detail['ri1_quantity'],
-					":ri1_actual_cost" => $detail['ri1_actual_cost'],
-					":ri1_new_cost" => $detail['ri1_new_cost'],
+					":ri1_actualcost" => $detail['ri1_actualcost'],
+					":ri1_newcost" => $detail['ri1_newcost'],
 					":ri1_increase_account" => $detail['ri1_increase_account'],
 					":ri1_declining_account" => $detail['ri1_declining_account'],
-					":ri1_ccost" => $detail['ri1_ccost'],
-					":ri1_ubussines" => $detail['ri1_ubussines'],
+					":ri1_costcode" => $detail['ri1_ccost'],
+					":ri1_ubusiness" => $detail['ri1_ubussines'],
 					":ri1_project" => $detail['ri1_project'],
-					":ri1_docentry" => $resInsert,				
-					":ri1_ubication" => $detail['ri1_ubication'],				
-					":ri1_total" => $detail['ri1_total']				
-				));
+					":ri1_docentry" => $resInsert,
+					":ri1_ubication" => $detail['ri1_ubication'],
+					":ri1_linetotal" => $detail['ri1_total']
+				)
+				);
 
 				if (is_numeric($resInsertDetail) && $resInsertDetail > 0) {
 					// Se verifica que el detalle no de error insertando //
@@ -121,9 +132,9 @@ class InventoryRevaluation extends REST_Controller {
 					$this->pedeo->trans_rollback();
 
 					$respuesta = array(
-						'error'   => true,
+						'error' => true,
 						'data' => $resInsertDetail,
-						'mensaje'	=> 'No se pudo registrar la revalorizacion'
+						'mensaje' => 'No se pudo registrar la revalorizacion'
 					);
 
 					$this->response($respuesta);
@@ -141,22 +152,50 @@ class InventoryRevaluation extends REST_Controller {
 			);
 
 			$this->response($respuesta);
-		 }else{
+		} else {
 			$this->pedeo->trans_rollback();
 
-				$respuesta = array(
-					'error'   => true,
-					'data' => $resInsert,
-					'mensaje'	=> 'No se pudo registrar la revalorizacion'
-				);
+			$respuesta = array(
+				'error' => true,
+				'data' => $resInsert,
+				'mensaje' => 'No se pudo registrar la revalorizacion'
+			);
 
-				$this->response($respuesta);
+			$this->response($respuesta);
 
-				return;
-		 }
+			return;
+		}
 
 
-		
-  }
+
+	}
+
+	public function getInvDetailDetail_get()
+	{
+		$Data = $this->get();
+		$sqlSelect = " SELECT * FROM iri1 WHERE ri1_docentry = :r1_docentry";
+
+		$resSelect = $this->pedeo->queryTable($sqlSelect, array(':r1_docentry' => $Data['ri1_docentry']));
+
+		if (isset($resSelect[0])) {
+
+			$respuesta = array(
+				'error' => false,
+				'data' => $resSelect,
+				'mensaje' => ''
+			);
+
+		} else {
+
+			$respuesta = array(
+				'error' => true,
+				'data' => array(),
+				'mensaje' => 'busqueda sin resultados'
+			);
+
+		}
+
+		$this->response($respuesta);
+	}
 
 }
